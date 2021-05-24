7ae1be8342405e53ced2e23847865ca28cd37d67
Anderson Sprenger <anderson.sprenger@edu.pucrs.br>
2021-05-24 17:48:13 -0300

    init

---
+++ LivingAlone/LivingAloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  LivingAloneApp.swift
+//  LivingAlone
+//
+//  Created by Anderson Sprenger on 24/05/21.
+//
+
+import SwiftUI
+
+@main
+struct LivingAloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
